981c8e4626bee97f3745312365a4bc0bfdb57376
unknown <minusa71@yahoo.com>
2022-04-03 09:23:40 +0300

    Added static files issue resolution

--- Cottage_garden_project/settings.py
+++ Cottage_garden_project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -122,10 +122,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
+#
+# STATICFILES_DIRS = (
+#     BASE_DIR / 'Cottage_garden_project/staticfiles',
+# )
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    BASE_DIR / 'Cottage_garden_project/staticfiles',
+    os.path.join(BASE_DIR, 'static'),
 )
 
 # Default primary key field type
