4b6cc2413dc25a6cda29794b990d5c38c8386e25
unknown <minusa71@yahoo.com>
2022-04-04 19:10:58 +0300

    Next one

--- Cottage_garden_project/main/views.py
+++ Cottage_garden_project/main/views.py
@@ -24,7 +24,11 @@ class HomeView(views.TemplateView):
 
 class UserDashboardView(views.ListView):
     model = Garden
+
     template_name = 'main/dashboard.html'
+
+    # def get_queryset(self):
+    #     return Garden.objects.all()
 
 
     def get_context_data(self, **kwargs):
