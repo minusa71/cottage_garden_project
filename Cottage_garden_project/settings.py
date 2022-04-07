6b8df87bd5ea775baabffd92d9718d3a5e42966f
unknown <minusa71@yahoo.com>
2022-04-07 13:10:24 +0300

    hide sensitive info

--- Cottage_garden_project/settings.py
+++ Cottage_garden_project/settings.py
@@ -27,9 +27,9 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', False) == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(' ')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split()
 
-APP_ENVIRONMENT=os.getenv('APP_ENVIRONMENT')
+APP_ENVIRONMENT = os.getenv('APP_ENVIRONMENT')
 
 # Application definition
 
