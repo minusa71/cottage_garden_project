ada00096f6d353ac69b739d1f01386cd42550e41
unknown <minusa71@yahoo.com>
2022-04-09 23:31:28 +0300

    Add cloudinary in settings4

--- Cottage_garden_project/main/admin.py
+++ Cottage_garden_project/main/admin.py
@@ -1,15 +1,24 @@
 from django.contrib import admin
 
 # Register your models here.
-from Cottage_garden_project.main.models import Profile, Plant
+from Cottage_garden_project.main.models import Profile, Plant, Garden
 
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
     pass
 
+
+
+@admin.register(Garden)
+class PlantAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Plant
+        fields = ('name', 'type', 'address', 'image')
+
+
 @admin.register(Plant)
 class PlantAdmin(admin.ModelAdmin):
     class Meta:
         model = Plant
-        fields = ('name', 'type', 'sort', 'image','year', 'harvest_quantity' , 'garden')
+        fields = ('name', 'type', 'sort', 'image','year', 'harvest_quantity', 'garden')
